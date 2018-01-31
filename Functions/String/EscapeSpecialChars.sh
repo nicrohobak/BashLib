cc64a7220d0343ab8689ee5c69669bd2c9f2127d
Nick Robertson <nrobertson@physics.ucla.edu>
2018-01-31 14:56:46 -0800

    EscapeSpecialChars now auto-detects characters that need escaping
    - BASHLIB_ESCAPE_CHAR_LIST stores a cache of characters that require
      escaping.  Emptying this variable can re-trigger the detection.
    
    - Exclaimation points are not handled because of the extensive
      cavats.  It seems that most problems with this will probably not
      make it into your variable in the first place.

--- Functions/String/EscapeSpecialChars.sh
+++ Functions/String/EscapeSpecialChars.sh
@@ -5,9 +5,17 @@
 #
 
 
-# Function:	EscapeSpecialChars -- Converts a string to lowercase
+# A global variable so this only has to be auto-detected when empty.  This
+# should help speed subsequent calls.  Simply empty this variable to force a
+# recalculation when necessary.
+BASHLIB_ESCAPE_CHAR_LIST=""
+
+
+# Function:	EscapeSpecialChars -- Backslash escapes special characters for use
+#                                 in double-quoted strings (but, does NOT
+#                                 attempt to handle exclaimation points!)
 # Params:	$* - The string
-# Output:	String (a lowercase version of the input)
+# Output:	String (The escaped version of the string)
 function EscapeSpecialChars
 {
 	if [ "${1}" = "" ]; then
@@ -15,7 +23,24 @@ function EscapeSpecialChars
 		return 1
 	fi
 
-	local SPECIAL_CHARACTER_LIST="'\"()-!?[]{}<>*&$:;\#|"
+	if [ "${BASHLIB_ESCAPE_CHAR_LIST}" = "" ]; then
+		# The exclaimation point is specially tricky and is thus not handled here...
+		# (The problem usually gets parsed out by the time this function is invoked.)
+		local SPECIAL_CHAR_LIST='@#$%^&*()_+[]{}|<>,./;:`'
+
+		# First, determine what even needs to be escaped with the
+		# help of $( print "%q" ... ) (Thanks codeforester of StackExchange)
+		for (( i = 0; i < ${#SPECIAL_CHAR_LIST}; ++i )); do
+			local TEST_CHAR="${SPECIAL_CHAR_LIST:i:1}"
+			local COMPARE_CHAR=""
+
+			printf -v COMPARE_CHAR "%q" "${TEST_CHAR}"
+
+			if [[ "${TEST_CHAR}" != "${COMPARE_CHAR}" ]]; then
+				BASHLIB_ESCAPE_CHAR_LIST="${BASHLIB_ESCAPE_CHAR_LIST}${TEST_CHAR}"
+			fi
+		done
+	fi
 
 	local ORIG_STR="$*"
 	local NEW_STR=""
@@ -24,9 +49,8 @@ function EscapeSpecialChars
 		local CUR_CHAR="${ORIG_STR:${i}:1}"
 
 		local FOUND_SPECIAL="false"
-		for (( s = 0; s < ${#SPECIAL_CHARACTER_LIST}; ++s ));
-	   	do
-			local CUR_SPECIAL="${SPECIAL_CHARACTER_LIST:${s}:1}"
+		for (( s = 0; s < ${#ESCAPE_CHAR_LIST}; ++s )); do
+			local CUR_SPECIAL="${ESCAPE_CHAR_LIST:${s}:1}"
 
 			if [ "${CUR_CHAR}" = "${CUR_SPECIAL}" ]; then
 				NEW_STR="${NEW_STR}"'\'"${CUR_SPECIAL}"
