3ad95113902a9981843a9f7dfa455499e1cfd00c
Nick Robertson <nrobertson@physics.ucla.edu>
2018-02-02 11:54:59 -0800

    EscapeStringChars now follows the rules outlined in the bash docs

--- Functions/String/EscapeStringChars.sh
+++ Functions/String/EscapeStringChars.sh
@@ -5,11 +5,10 @@
 #
 
 
-# Function:	EscapeStringChars -- Backslash escapes special characters for use
-#                                in double-quoted strings (but, does NOT
-#                                attempt to handle exclamation points!)
-# Params:	$* - The string
-# Output:	String (The escaped version of the string)
+# Function: EscapeStringChars -- Backslash escapes special characters for use
+#                                in double-quoted strings
+# Params:   $* - The string
+# Output:   String (The escaped version of the string)
 function EscapeStringChars
 {
 	if [ "${1}" = "" ]; then
@@ -17,30 +16,30 @@ function EscapeStringChars
 		return 1
 	fi
 
-	# The exclamation point is specially tricky and is thus not handled here...
-	# (The problem usually gets parsed out by the time this function is invoked.)
-	local SPECIAL_CHAR_LIST='$`\"'
-
 	local ORIG_STR="$*"
 	local NEW_STR=""
+	local PREV_CHAR=""
 
 	for (( i = 0; i < ${#ORIG_STR}; ++i )); do
 		local CUR_CHAR="${ORIG_STR:${i}:1}"
 
-		local FOUND_SPECIAL="false"
-		for (( s = 0; s < ${#SPECIAL_CHAR_LIST}; ++s )); do
-			local CUR_SPECIAL="${SPECIAL_CHAR_LIST:${s}:1}"
+		case "${CUR_CHAR}" in
+			'!'|'$'|'`'|'"')
+				if [ "${PREV_CHAR}" = '\' ]; then
+					NEW_STR="${NEW_STR}${CUR_CHAR}"
+				else
+					NEW_STR="${NEW_STR}\\${CUR_CHAR}"
+				fi
+				;;
+			'\')
+				NEW_STR="${NEW_STR}\\"
+				;;
+			*)
+				NEW_STR="${NEW_STR}${CUR_CHAR}"
+				;;
+		esac
 
-			if [ "${CUR_CHAR}" = "${CUR_SPECIAL}" ]; then
-				NEW_STR="${NEW_STR}"'\'"${CUR_SPECIAL}"
-				FOUND_SPECIAL="true"
-				break
-			fi
-		done
-
-		if [ "${FOUND_SPECIAL}" != "true" ]; then
-			NEW_STR="${NEW_STR}${CUR_CHAR}"
-		fi
+		PREV_CHAR="${CUR_CHAR}"
 	done
 
 	echo "${NEW_STR}"
