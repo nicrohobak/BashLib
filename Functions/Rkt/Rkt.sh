f5de7d176fbe64dc202a6eba70777dcd4b037d03
Nick Robertson <nrobertson@physics.ucla.edu>
2018-11-27 15:53:16 -0800

    Added a new Rkt-related command
    - RktUpdateImages is a special 'rkt fetch' that adjusts permissions
      on the fetched image to allow for more non-root access

--- Functions/Rkt/Rkt.sh
+++ Functions/Rkt/Rkt.sh
@@ -1,47 +1,55 @@
+#!/bin/bash
+
 #
 # Rkt
 #
 
-# Use these variables to provide your own custom options to these commands
-export RKT="rkt"
-export RKT_DATA_DIR="/var/lib/rkt"
+# General rkt data locations
+RKT_DATA_DIR="/var/lib/rkt"
+RKT_IMAGE_PATH="${RKT_DATA_DIR}/cas/blob/sha512"        # rkt fetch storage location
 
-export ACBUILD="acbuild --debug"
+
+# Use these variables to provide your own custom options to these commands
+
+RKT="rkt --dir=${RKT_DATA_DIR}"
+
+ACBUILD="acbuild --debug"
 
 
 #
 # DANGER: This directory location is subject to root-level "rm -rf"!!
 #
 # The acbuild temporary build directory (acbuild default is './.acbuild')
-export RKT_ACBUILD_TMP="./.acbuild"
+RKT_ACBUILD_TMP="./.acbuild"
 #
 # DANGER: This directory location is subject to root-level "rm -rf"!!
 #
 
-export RKT_ACBUILD_ROOTFS="${RKT_ACBUILD_TMP}/currentaci/rootfs"
+RKT_ACBUILD_ROOTFS="${RKT_ACBUILD_TMP}/currentaci/rootfs"
 
 
 # Important Container Path Information
-export RKT_DEFAULT_ENTRY_PATH="/usr/local/sbin"
-export RKT_DEFAULT_ENTRY_POINT="container-entry-point"
-export RKT_CONTAINER_ENTRY_POINT="${RKT_DEFAULT_ENTRY_PATH}/${RKT_DEFAULT_ENTRY_POINT}"
+RKT_DEFAULT_ENTRY_PATH="/usr/local/sbin"
+RKT_DEFAULT_ENTRY_POINT="container-entry-point"
+RKT_CONTAINER_ENTRY_POINT="${RKT_DEFAULT_ENTRY_PATH}/${RKT_DEFAULT_ENTRY_POINT}"
 
 
 #
 # Rkt Container package management assistance
 #
-export RKT_PKGMGR_LIST="default apk apt-get yum pacman"
-export RKT_PKGMGR_DEFAULT="apk"
+RKT_PKGMGR_LIST="default apk apt-get yum pacman"
+RKT_PKGMGR_DEFAULT="apk"
 
 # Package manager "install" commands
 declare -A RKT_PKGMGR_INSTALL
 RKT_PKGMGR_INSTALL["default"]="install"
-RKT_PKGMGR_INSTALL["apk"]="add"
+RKT_PKGMGR_INSTALL["apk"]="add --no-cache"
 RKT_PKGMGR_INSTALL["pacman"]="-S"
 
 # Package manager "update" commands
 declare -A RKT_PKGMGR_UPDATE
 RKT_PKGMGR_UPDATE["default"]="update"
+RKT_PKGMGR_UPDATE["apk"]="update --no-cache"
 RKT_PKGMGR_UPDATE["pacman"]="-Syy"
 
 # Package manager "clean" commands
---
+++ Functions/Rkt/UpdateImages.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#
+# RktUpdateImages
+#
+
+
+# Default base-images to fetch
+RKT_IMAGE_LIST="docker://alpine:latest"
+
+
+# Function: RktUpdateImages
+# Params:   $* - Images to fetch (in addition to the base list)
+# Output:   (Progress / Status)
+function RktUpdateImages
+{
+	local IMAGE_LIST="${*} ${RKT_IMAGE_LIST}"
+
+	if [ "${IMAGE_LIST}" = "" ]; then
+	    Warn "RktUpdateImages: No images to fetch.  Nothing to do."
+	    exit 0
+	fi
+
+	local IMAGE
+	for IMAGE in ${IMAGE_LIST}; do
+		Print "Fetching '${IMAGE}'..."
+
+		# Explictly allow insecure images for wider compatibility
+		${RKT} --insecure-options=image fetch "${IMAGE}"
+
+		if [ "${?}" != "0" ]; then
+			Error "RktUpdateImages: Failed to fetch '${IMAGE}'."
+			return 1
+		fi
+	done	
+
+	Print -n "Adjusting local image permissions to allow read access for 'rkt' group..."
+	chmod -R g+r "${RKT_IMAGE_PATH}"
+
+	if [ "${?}" != "0" ]; then
+		Warn "RktUpdateImages: Failed to set group read permissions on everything within '${RKT_IMAGE_PATH}'.  Attempting again with 'sudo'..."
+		sudo chmod -R g+r "${RKT_IMAGE_PATH}"
+
+		if [ "${?}" != "0" ]; then
+			Error "RktUpdateImages: Failed to set group read permissoins on '${RKT_IMAGE_PATH}'."
+			return 1
+		else
+			Print "Done."
+		fi
+	else
+		Print "Done."
+	fi
+
+	Print "All images fetched successfully."
+	return 0
+}
+
+
+# vim: tabstop=4 shiftwidth=4
+
+
+
